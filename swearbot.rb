5cac4272deb41008c2e7d24743def0b54113aea5
geomese <geomese@gmail.com>
2013-10-30 21:47:04 -0400

    added uptime and current time

--- swearbot.rb
+++ swearbot.rb
@@ -1,5 +1,8 @@
 #boring swearbot that reprimands a user for swearing
 
+#need to fix help command
+
+
 
 require "socket"
 
@@ -9,6 +12,8 @@ nick = "SwearBot"
 channel = "#bojangles"
 prefix = "privmsg #{channel} :"
 is_curse = ["fuck", "shit", "ass", "hell", "damn"]
+dat_uptime = "uptime"
+current_time = "whattime"
 
 irc_server = TCPSocket.open(server, port)
 
@@ -35,6 +40,19 @@ until irc_server.eof? do
   
     reprimand = "Keep it clean!"
     irc_server.puts "PRIVMSG #{channel} :#{reprimand}"
+  end
+
+
+  if msg.include?(dat_uptime)
+    startup = Time.new
+    runtime = "I've been running since #{startup.strftime("%I:%M%p %d %B %Y")} for a total of #{(Time.now - startup)/60/60/24} days."
+    irc_server.puts "PRIVMSG #{channel} :#{runtime}"
+  elsif msg.include?(current_time)
+    thetime = "It is currently #{Time.now.strftime("%I:%M%p %d %B %Y")}"
+    irc_server.puts "PRIVMSG #{channel} :#{thetime}"
+  #elsif msg.include?(bot_do) && (plzhelp)
+  #  helpme = "Type \!swear uptime for current runtime of bot, \!swear time for current time."
+  #  irc_server.puts "PRIVMSG #{channel} :#{helpme}"
   end
 
 end
